193031eebf07b318ba2f3e13a2b256ac8c566e67
Namwoosung <142873609+Namwoosung@users.noreply.github.com>
2025-04-10 16:51:23 +0900

    add basic setting for spring server

---
+++ spring_server/src/test/java/com/SyncBoard/spring_server/SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SyncBoard.spring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
